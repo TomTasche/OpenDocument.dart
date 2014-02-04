1b7f1670f9e9e42ebbec9c1024414c0d16604d00
Thomas Taschauer <tomtasche@gmail.com>
2014-02-04 21:14:39 +0100

    refactor code

--- web/opendocument.dart
+++ web/opendocument.dart
@@ -12,24 +12,26 @@ void main() {
   fileInput.addEventListener("change", onFileChanged);
 }
 
-void onFileChanged(var event) {
+void onFileChanged(Event event) {
   FileList files = fileInput.files;
   var file = files.item(0);
 
   FileReader reader = new FileReader();
-  reader.addEventListener("load", (fileEvent) {
-    Archive archive = new ZipDecoder().decodeBytes(reader.result);
-
-    for (int i = 0; i < archive.numberOfFiles(); ++i) {
-      String filename = archive.fileName(i);
-      List<int> data = archive.fileData(i);
-      if (filename == "content.xml") {
-        String content = new String.fromCharCodes(data);
-        
-        resultDiv.text = content;
-      }
-    }
-  });
-  
+  reader.addEventListener("load", onZipLoaded);
   reader.readAsArrayBuffer(file);
+}
+
+void onZipLoaded(ProgressEvent event) {
+  FileReader reader = event.currentTarget;
+  Archive archive = new ZipDecoder().decodeBytes(reader.result);
+  
+  for (int i = 0; i < archive.numberOfFiles(); ++i) {
+    String filename = archive.fileName(i);
+    List<int> data = archive.fileData(i);
+    if (filename == "content.xml") {
+      String content = new String.fromCharCodes(data);
+      
+      resultDiv.text = content;
+    }
+  }
 }
